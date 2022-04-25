6cc7d4cbf51990926b10107944e64ec5df05b7d6
Woo joo Chae <wchae@c2r1s7.42seoul.kr>
2022-04-25 20:26:05 +0900

    reverse

--- 03Stack/03-3ReverseString/reverse_string.c
+++ 03Stack/03-3ReverseString/reverse_string.c
@@ -1,8 +1,44 @@
+#include <string.h>
 #include "linkedstack.h"
 
+char    *reverse_string(char    *str)
+{
+    char    *reversed;
+    int     i;
+    int     str_len;
 
+    LinkedStack *stack;
+    StackNode   node;
+
+    if (!str)
+        return NULL;
+    str_len = strlen(str);
+    reversed = (char *)malloc(sizeof(char) *str_len + 1);
+    
+    stack = createLinkedStack();
+    for (int i = 0; i < str_len; i++)
+    {
+         node.data = str[i];
+         push(stack, node);
+    }
+    for (int i = 0; i < str_len; i++)
+    {
+        StackNode *poped;
+        poped = pop(stack);
+        reversed[i] = poped->data;
+        free(poped);
+    }
+    deleteLinkedStack(stack);
+    return (reversed);
+}
 
 int main()
 {
-
+    char    *str = "ABCDEFG";
+    char    *reversed;
+    printf("original = %s\n", str);
+    reversed = reverse_string(str);
+    printf("reversed = %s\n", reversed);
+    free(reversed);
+    system("leaks a.out");
 }
---
+++ 03Stack/03-4CheckBracket/check_bracket.c
@@ -0,0 +1,69 @@
+#include "linkedstack.h"
+/*
+( ( A * B ) / C ) - { ( D + E ) && ( F – G ) } 
+
+( ( A * B ) / C - { ( D + E ) && ( F – G ) ) }
+
+*/
+
+static int  is_open_bracket(int c)
+{
+    if (c == '(' || c == '[' || c == '{')
+        return (TRUE);
+    return (FALSE);
+}
+
+static int  is_close_bracket(int c)
+{
+    if (c == ')' || c == ']' || c == '}')
+        return (TRUE);
+    return (FALSE);
+}
+
+int checkBracketMatching(char *exp)
+{
+    LinkedStack *stack = createLinkedStack();
+    StackNode   node;
+    StackNode   *poped;
+    int flag;
+    int c;
+
+    while (*exp)
+    {
+        if (is_open_bracket(*exp))
+        {
+            node.data = *exp;
+            push(stack, node);
+        }
+        else if (is_close_bracket(*exp))
+        {
+            poped = pop(stack);
+            c = poped->data;
+            free(poped);
+            deleteLinkedStack(stack);
+            if(!c)
+                return (FALSE);
+            else
+            {
+                if (c == '(' && *exp != ')')
+                    return (FALSE);
+                if (c == '{' && *exp != '}')
+                    return (FALSE);
+                if (c == '[' && *exp != ']')
+                    return (FALSE);
+            }
+        }
+        exp++;
+    }
+    return (TRUE);
+}
+
+int main(void)
+{
+    char    *expression = "(( 20 * 30 ) / 10) - { ( D + E ) && ( F - G) } ";
+    char    *wrong = "(( 20 * 30 ) / 10 - { ( D + E ) && ( F - G)) } ";
+
+    printf("OK = %d\n", checkBracketMatching(expression));
+    printf("X = %d\n", checkBracketMatching(wrong));
+    system("leaks a.out");
+}
