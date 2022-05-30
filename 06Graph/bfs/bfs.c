9fe03fec42ab4fc428fc9dc3b768a8bab95e1fb5
Woo Joo Chae <enaenen@naver.com>
2022-05-30 21:19:18 +0900

    comment

--- 06Graph/bfs/bfs.c
+++ 06Graph/bfs/bfs.c
@@ -14,11 +14,16 @@ void bfs(LinkedGraph *pGraph, int startVertex)
 	{
 		DequeNode *current;
 		current = deleteRearLD(pDeque);
-		//여기에서 체크를 해 줄  경우 2번 노드를 방문했을때 2번과 연결된 5번 노드를 enqueue
-		// 이후, 4번 노드까지 진행되었을때, 4번도 5번과 연결되어있으므로 5번 노드가 한번 더 enqueue됨
-		// 따라서, enqueue하기 전에 방문을 했다고 체크를 해주어야 함
-
-		// pVisited[current->data.vertexID] = USED;
+		/* Visited 체크 위치 중요 -> 여기서 체크를 했더니 5가 중복되는 현상 발생
+		Visited 는 꺼내서 출력할때가 아니라 넣을때 이미 방문 체크를 해주어야함.
+		
+			 2번 노드를 방문했을때 2번과 연결된 5번 노드를 enqueue
+			 이후, 4번 노드까지 진행되었을때, 4번도 5번과 연결되어있으므로 5번 노드가 한번 더 enqueue됨
+			 5번을 한번 꺼냈었으면 그때 Visited를 체크
+			 따라서, enqueue하기 전에 방문을 했다고 체크를 해주어야 함
+			 
+		 pVisited[current->data.vertexID] = USED;
+		*/
 		printf(" %d ", current->data.vertexID);
 		if (current)
 		{
