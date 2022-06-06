05efcef70ace43d985c76fcfdcaf9f4b23625abe
Woo Joo Chae <enaenen@naver.com>
2022-06-06 20:00:26 +0900

    p

--- 06Graph/algorithym/dijkstra/prac/dijkstra2.c
+++ 06Graph/algorithym/dijkstra/prac/dijkstra2.c
@@ -91,6 +91,8 @@ int	*shortestPathDijkstra(LinkedGraph *pGraph, int startVertexId)
 		{
 			int toVertexId = pListNode->data.vertexID;
 			int weight = pListNode->data.weight;
+			// 다시보기 2022-0606
+			// 이게맞나?
 			//시작노드->가중치가 가장짧은 두번째노드
 			//까지의 가중치 (0->1 ---2)
 			y_v = pReturn[vertexId];
